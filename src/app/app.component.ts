6e7009f86688be1cb50b0bbb5974466f774e0ddb
Jessica Costa <jessicacosta.dev@hotmail.com>
2020-01-13 14:00:45 -0300

    [Menu] Ocultando menu quando a página for de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  href: string;
   title = 'app-angular-crm';
   opened = true;
+  hideMenu = false;
 
-  constructor(translate: TranslateService) {
+  constructor(translate: TranslateService, private location: Location) {
     // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('pt-br');
 
      // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use('pt-br');
   }
+
+  ngOnInit() {
+    this.href = this.location.path();
+    if (this.href === '/' || this.href === '/login') {
+      this.opened = false;
+      this.hideMenu = true;
+    }
+  }
+
 }
